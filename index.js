b16df97cfc4949fabef818084dbaec2f014cb423
EduardaMachadoSevero <eduarda.smachado@hotmail.com>
2020-10-27 20:39:00 -0300

    Criação da rota Layout

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ app.get('/',function(req,res){
 app.get('/msg', function(req,res){
     res.send("Essa mensagem é automática")
 })
-
+app.get('/layout', function(req,res){
+    res.send("Esse é o novo Layout!")
+})
 app.listen(3000, function(){
 
     console.log('Conexão inicializada')

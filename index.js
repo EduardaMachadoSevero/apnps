59b0f863e0564c7d01bfa1f04827fa2e35e1d9cf
EduardaMachadoSevero <eduarda.smachado@hotmail.com>
2020-10-27 20:35:14 -0300

    Adicionando a rota sobre

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ app.get('/msg', function(req,res){
     res.send("Essa mensagem é automática")
 })
 
+app.get('/sobre', function(req,res){
+    res.send("Esta página está sendo desenvolvida")
+})
+
 app.listen(3000, function(){
 
     console.log('Conexão inicializada')
